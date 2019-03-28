2f9a3342357362e4365a05445be8570f8524d29f
fabianBHeredia <heredia800@gmail.com>
2019-03-28 18:36:38 -0500

    saliendo json

--- src/main/resources/static/weatherApp.js
+++ src/main/resources/static/weatherApp.js
@@ -1,14 +1,18 @@
 var cityInfo;
-var weatherApp =(function(){
-    return{
-        searchButton:function(){
+var weatherApp = (function () {
+    return {
+        searchButton: function () {
             console.log("testing this s out");
             var city = $("#city").val();
             console.log(city);
-            $.get('/cities/'+city,function(cityInfo){
+            $.get('/cities/' + city, function (cityInfo) {
                 console.log(cityInfo);
+                var obj = JSON.parse(cityInfo);
+                console.log(obj);
+                $("#tabla").find('tbody').append(cityInfo);
+
             });
-            
+
         }
     }
 })();
