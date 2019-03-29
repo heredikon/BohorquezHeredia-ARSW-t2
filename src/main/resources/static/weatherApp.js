e72660e22a86f1d6b76479f3e4c16e3beda08b9a
fabianBHeredia <heredia800@gmail.com>
2019-03-28 19:11:30 -0500

    saliendo json2

--- src/main/resources/static/weatherApp.js
+++ src/main/resources/static/weatherApp.js
@@ -8,11 +8,9 @@ var weatherApp = (function () {
             $.get('/cities/' + city, function (cityInfo) {
                 console.log(cityInfo);
                 var obj = JSON.parse(cityInfo);
+
                 console.log(obj);
-                document.getElementById("demo").innerHTML = 
-                "<table><tr>" 
-                "name: " + obj.coord.lon +
-                "</tr></table>"
+                $("#tabla").find("tbody").append(JSON.stringify();
 
             });
 
